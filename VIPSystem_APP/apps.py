99282c8a6a72967aa4ea54acb2cd034fb5fc2959
STRfong <ofng@strnetwork.cc>
2024-09-12 22:55:39 +0800

    fixed: warning from db

--- VIPSystem_APP/apps.py
+++ VIPSystem_APP/apps.py
@@ -1,10 +1,13 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def initialize_social_app(sender, **kwargs):
+    from .models import CustomSocialApp
+    CustomSocialApp.get_or_create_app('google')
 
 class VipsystemAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "VIPSystem_APP"
 
     def ready(self):
-        from .models import CustomSocialApp
-        CustomSocialApp.get_or_create_app('google')
+        post_migrate.connect(initialize_social_app, sender=self)
