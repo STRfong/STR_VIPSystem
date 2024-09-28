938822f7b308753661c55ecbcd7c928ce9dae369
STRfong <ofng@strnetwork.cc>
2024-09-29 00:57:45 +0800

    增加網頁響應，並新增了一些保護措施

--- VIPSystem/views.py
+++ VIPSystem/views.py
@@ -21,34 +21,3 @@ def register(request):
         form = UserCreationForm()
         context = {'form': form}
         return render(request, 'registration/register.html', context)
-
-
-def send_email(request):
-    if request.method == 'POST':
-        try:
-            with smtplib.SMTP(host="smtp.gmail.com", port="587") as smtp:
-                smtp.ehlo()
-                smtp.starttls()
-                smtp.login(os.getenv('EMAIL_HOST_USER'), os.getenv('EMAIL_HOST_PASSWORD'))
-                
-                msg = MIMEMultipart('alternative')
-                msg['From'] = "lab@strnetwork.cc"
-                msg['To'] = request.POST['sender']
-                msg['Subject'] = "測試郵件"
-                
-                # 渲染 HTML 模板
-                html_content = render_to_string(
-                    'email_template.html',
-                    {'username': request.user.username, 'content': request.POST['content']}
-                )
-                
-                # 添加 HTML 内容到邮件
-                msg.attach(MIMEText(html_content, 'html'))
-                
-                smtp.send_message(msg)
-                print("完成!")
-        except Exception as e:
-            print("錯誤訊息: ", e)
-        
-        return redirect('/')
-    return render(request, 'send_email.html')
