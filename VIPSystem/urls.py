94dfd63b318ba9b92134a577d4a042ccdf4f4cb4
STRfong <ofng@strnetwork.cc>
2024-12-17 11:02:19 +0800

    feature: 以帳號密碼登入

--- VIPSystem/urls.py
+++ VIPSystem/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from .views import new_user_profile_profile_form
+from . import views
+from .views import new_user_profile_profile_form, login_view
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('login/', views.login_view, name='login'),
     path('VIPSystem_APP/', include('VIPSystem_APP.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
